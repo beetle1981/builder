80d32e6a33dec8bafead9ae8c3c3628f68844b10
Roy Lee <25062566@qq.com>
2026-09-02 08:55:18 +0000

    update addon version and shell scripts

--- xupdate.sh
+++ xupdate.sh
@@ -6,6 +6,14 @@ export XADDONSDIR=$XDIR/package/addons
 FEEDSDIR=$XDIR/package/feeds
 ADDONSCFG=$XDIR/../_addons.config
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color（重置颜色，必须加在文末，否则后面的输出都会变色）
+
 . ./xcommon.sh
 
 OPT_FULL_UPDATE=false
@@ -40,6 +48,8 @@ CUR_BRANCH=$( git rev-parse --abbrev-ref HEAD )
 git reset --hard origin/$CUR_BRANCH
 [ "$?" != "0" ] && die "Can't reset current repository"
 
+echo -e "${GREEN}SUCCESS:${NC} config feeds!"
+
 rm -f feeds.conf
 cp -f feeds.conf.default feeds.conf
 feed_lst=$( get_cfg_feed_lst "$ADDONSCFG" )
@@ -49,10 +59,18 @@ for feed in $feed_lst; do
 	echo "src-git $feed $value" >> feeds.conf
 done
 
+echo -e "${GREEN}SUCCESS:${NC} update feeds A!!"
+
 if [ "$OPT_FULL_UPDATE" = "true" ]; then
 	./scripts/feeds update -a
 	./scripts/feeds install -a
+	rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+	rm -rf package/addons/passwall_packages
+	git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/addons/passwall_packages
+	./scripts/feeds install -a
 fi
+
+echo -e "${GREEN}SUCCESS:${NC} update feeds B!!"
 
 CLONE_ADDONS=true
 if [ "$CLONE_ADDONS" = "true" ]; then
@@ -74,6 +92,8 @@ if [ "$CLONE_ADDONS" = "true" ]; then
 	fi
 fi
 
+echo -e "${GREEN}SUCCESS:${NC} update vermagic!!"
+
 if [ "$OPT_FULL_UPDATE" = "true" ]; then
 	if [ -f "$XDIR/vermagic_update.sh" ]; then
 		./vermagic_update.sh ipq806x generic
@@ -86,4 +106,4 @@ if [ -f "$XDIR/luci_dispatcher.sh" ]; then
 	./luci_dispatcher.sh
 fi
 
-echo "All git sources updated!"
+echo -e "${GREEN}SUCCESS:${NC} All git sources updated!"
